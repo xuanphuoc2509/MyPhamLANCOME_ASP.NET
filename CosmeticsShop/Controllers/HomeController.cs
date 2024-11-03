c36b80365c7eda95a9665352ddecb1c514e1f60d
xuanphuoc2509 <xuanphuoc2509@gmail.com>
2024-11-03 18:46:29 +0700

    Initial commit

---
+++ CosmeticsShop/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using CosmeticsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmeticsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShoppingEntities1 db = new ShoppingEntities1();
+        public ActionResult Index()
+        {
+            if (Session["Cart"] == null)
+            {
+                Session["Cart"] = new List<ItemCart>();
+            }
+            ViewBag.ListProduct = db.Products.Where(x => x.IsActive == true && x.PurchasedCount > 0).OrderByDescending(x => x.PurchasedCount).ToList();
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(Models.User user)
+        {
+            Models.User check = db.Users.SingleOrDefault(x => x.Email == user.Email);
+            if (check != null)
+            {
+                ViewBag.Message = "Email đã tồn tại";
+                return View();
+            }
+
+            Models.User userAdded = new Models.User();
+            try
+            {
+                user.Captcha = new Random().Next(100000, 999999).ToString();
+                user.IsConfirm = true;
+                user.UserTypeID = 2;
+                user.Address = "user3.jpg";
+                userAdded = db.Users.Add(user);
+                db.SaveChanges();
+               
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Đăng ký thất bại";
+                return View();
+            }
+            return RedirectToAction("SignIn", "Home", new { ID = userAdded.ID });
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(string Email, string Password)
+        {
+            Models.User check = db.Users.SingleOrDefault(x => x.Email == Email && x.Password == Password);
+            if (check != null)
+            {
+                Session["User"] = check;
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Message = "Email hoặc mật khẩu không đúng";
+            return View();
+        }
+        public ActionResult SignOut()
+        {
+            Session.Remove("User");
+            return RedirectToAction("Index");
+        }
+    }
+}
